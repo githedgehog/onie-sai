3bcbc4fefea9eb8a4d49f4d1926a29b7110d1f40
Marcus Heese <marcus@githedgehog.com>
2023-09-11 22:24:22 -0700

    using default in test in sai-sys

--- sai-sys/src/lib.rs
+++ sai-sys/src/lib.rs
@@ -93,61 +93,13 @@ mod tests {
             assert_eq!(st, SAI_STATUS_SUCCESS as i32);
 
             // query available functionality
-            let mut apis: sai_apis_t = sai_apis_t { 
-                switch_api: std::ptr::null_mut(),
-                port_api: std::ptr::null_mut(),
-                fdb_api: std::ptr::null_mut(),
-                vlan_api: std::ptr::null_mut(),
-                virtual_router_api: std::ptr::null_mut(),
-                route_api: std::ptr::null_mut(),
-                next_hop_api: std::ptr::null_mut(),
-                next_hop_group_api: std::ptr::null_mut(),
-                router_interface_api: std::ptr::null_mut(),
-                neighbor_api: std::ptr::null_mut(),
-                acl_api: std::ptr::null_mut(),
-                hostif_api: std::ptr::null_mut(),
-                mirror_api: std::ptr::null_mut(),
-                samplepacket_api: std::ptr::null_mut(),
-                stp_api: std::ptr::null_mut(),
-                lag_api: std::ptr::null_mut(),
-                policer_api: std::ptr::null_mut(),
-                wred_api: std::ptr::null_mut(),
-                qos_map_api: std::ptr::null_mut(),
-                queue_api: std::ptr::null_mut(),
-                scheduler_api: std::ptr::null_mut(),
-                scheduler_group_api: std::ptr::null_mut(),
-                buffer_api: std::ptr::null_mut(),
-                hash_api: std::ptr::null_mut(),
-                udf_api: std::ptr::null_mut(),
-                tunnel_api: std::ptr::null_mut(),
-                l2mc_api: std::ptr::null_mut(),
-                ipmc_api: std::ptr::null_mut(),
-                rpf_group_api: std::ptr::null_mut(),
-                l2mc_group_api: std::ptr::null_mut(),
-                ipmc_group_api: std::ptr::null_mut(),
-                mcast_fdb_api: std::ptr::null_mut(),
-                bridge_api: std::ptr::null_mut(),
-                tam_api: std::ptr::null_mut(),
-                srv6_api: std::ptr::null_mut(),
-                mpls_api: std::ptr::null_mut(),
-                dtel_api: std::ptr::null_mut(),
-                bfd_api: std::ptr::null_mut(),
-                isolation_group_api: std::ptr::null_mut(),
-                nat_api: std::ptr::null_mut(),
-                counter_api: std::ptr::null_mut(),
-                debug_counter_api: std::ptr::null_mut(),
-                macsec_api: std::ptr::null_mut(),
-                system_port_api: std::ptr::null_mut(),
-                my_mac_api: std::ptr::null_mut(),
-                ipsec_api: std::ptr::null_mut(),
-                bmtor_api: std::ptr::null_mut(),
-            };
+            let mut apis: sai_apis_t = Default::default();
             let _st = sai_metadata_apis_query(Some(sai_api_query), &mut apis);
 
             // it's expected that some of the api queries will actually fail
             // so we cannot rely on a success result here
             // assert_eq!(st, SAI_STATUS_SUCCESS as i32);
-            
+
             // however, there are some APIs which must be there
             // in fact we should simply check for all of them which we care for
             assert_ne!(apis.switch_api, std::ptr::null_mut());
