a24fd342adf25981056c62b2db6a748716cbcd17
Marcus Heese <marcus@githedgehog.com>
2023-09-11 23:36:36 -0700

    adding API version and log set APIs

--- sai/src/lib.rs
+++ sai/src/lib.rs
@@ -137,7 +137,7 @@ impl From<Status> for InitError {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Status {
     Success,
     Failure,
@@ -211,12 +211,238 @@ impl From<sai_status_t> for Status {
     }
 }
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum LogLevel {
+    Debug,
+    Info,
+    Notice,
+    Warn,
+    Error,
+    Critical,
+}
+
+impl From<LogLevel> for sai_log_level_t {
+    fn from(value: LogLevel) -> Self {
+        match value {
+            LogLevel::Debug => _sai_log_level_t_SAI_LOG_LEVEL_DEBUG,
+            LogLevel::Info => _sai_log_level_t_SAI_LOG_LEVEL_INFO,
+            LogLevel::Notice => _sai_log_level_t_SAI_LOG_LEVEL_NOTICE,
+            LogLevel::Warn => _sai_log_level_t_SAI_LOG_LEVEL_WARN,
+            LogLevel::Error => _sai_log_level_t_SAI_LOG_LEVEL_ERROR,
+            LogLevel::Critical => _sai_log_level_t_SAI_LOG_LEVEL_CRITICAL,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum API {
+    Unspecified,
+    Switch,
+    Port,
+    FDB,
+    VLAN,
+    VirtualRouter,
+    Route,
+    NextHop,
+    NextHopGroup,
+    RouterInterface,
+    Neighbor,
+    ACL,
+    HostIf,
+    Mirror,
+    SamplePacket,
+    STP,
+    LAG,
+    Policer,
+    WRED,
+    QosMap,
+    Queue,
+    Scheduler,
+    SchedulerGroup,
+    Buffer,
+    Hash,
+    UDF,
+    Tunnel,
+    L2MC,
+    IPMC,
+    RpfGroup,
+    L2mcGroup,
+    IpmcGroup,
+    McastFdb,
+    Bridge,
+    TAM,
+    SRV6,
+    MPLS,
+    DTEL,
+    BFD,
+    IsolationGroup,
+    NAT,
+    Counter,
+    DebugCounter,
+    MACSEC,
+    SystemPort,
+    MyMac,
+    IPSEC,
+    GenericProgrammable,
+    Unknown(u32),
+}
+
+impl From<sai_api_t> for API {
+    fn from(value: sai_api_t) -> Self {
+        match value {
+            sai_sys::_sai_api_t_SAI_API_UNSPECIFIED => API::Unspecified,
+            sai_sys::_sai_api_t_SAI_API_SWITCH => API::Switch,
+            sai_sys::_sai_api_t_SAI_API_PORT => API::Port,
+            sai_sys::_sai_api_t_SAI_API_FDB => API::FDB,
+            sai_sys::_sai_api_t_SAI_API_VLAN => API::VLAN,
+            sai_sys::_sai_api_t_SAI_API_VIRTUAL_ROUTER => API::VirtualRouter,
+            sai_sys::_sai_api_t_SAI_API_ROUTE => API::Route,
+            sai_sys::_sai_api_t_SAI_API_NEXT_HOP => API::NextHop,
+            sai_sys::_sai_api_t_SAI_API_NEXT_HOP_GROUP => API::NextHopGroup,
+            sai_sys::_sai_api_t_SAI_API_ROUTER_INTERFACE => API::RouterInterface,
+            sai_sys::_sai_api_t_SAI_API_NEIGHBOR => API::Neighbor,
+            sai_sys::_sai_api_t_SAI_API_ACL => API::ACL,
+            sai_sys::_sai_api_t_SAI_API_HOSTIF => API::HostIf,
+            sai_sys::_sai_api_t_SAI_API_MIRROR => API::Mirror,
+            sai_sys::_sai_api_t_SAI_API_SAMPLEPACKET => API::SamplePacket,
+            sai_sys::_sai_api_t_SAI_API_STP => API::STP,
+            sai_sys::_sai_api_t_SAI_API_LAG => API::LAG,
+            sai_sys::_sai_api_t_SAI_API_POLICER => API::Policer,
+            sai_sys::_sai_api_t_SAI_API_WRED => API::WRED,
+            sai_sys::_sai_api_t_SAI_API_QOS_MAP => API::QosMap,
+            sai_sys::_sai_api_t_SAI_API_QUEUE => API::Queue,
+            sai_sys::_sai_api_t_SAI_API_SCHEDULER => API::Scheduler,
+            sai_sys::_sai_api_t_SAI_API_SCHEDULER_GROUP => API::SchedulerGroup,
+            sai_sys::_sai_api_t_SAI_API_BUFFER => API::Buffer,
+            sai_sys::_sai_api_t_SAI_API_HASH => API::Hash,
+            sai_sys::_sai_api_t_SAI_API_UDF => API::UDF,
+            sai_sys::_sai_api_t_SAI_API_TUNNEL => API::Tunnel,
+            sai_sys::_sai_api_t_SAI_API_L2MC => API::L2MC,
+            sai_sys::_sai_api_t_SAI_API_IPMC => API::IPMC,
+            sai_sys::_sai_api_t_SAI_API_RPF_GROUP => API::RpfGroup,
+            sai_sys::_sai_api_t_SAI_API_L2MC_GROUP => API::L2mcGroup,
+            sai_sys::_sai_api_t_SAI_API_IPMC_GROUP => API::IpmcGroup,
+            sai_sys::_sai_api_t_SAI_API_MCAST_FDB => API::McastFdb,
+            sai_sys::_sai_api_t_SAI_API_BRIDGE => API::Bridge,
+            sai_sys::_sai_api_t_SAI_API_TAM => API::TAM,
+            sai_sys::_sai_api_t_SAI_API_SRV6 => API::SRV6,
+            sai_sys::_sai_api_t_SAI_API_MPLS => API::MPLS,
+            sai_sys::_sai_api_t_SAI_API_DTEL => API::DTEL,
+            sai_sys::_sai_api_t_SAI_API_BFD => API::BFD,
+            sai_sys::_sai_api_t_SAI_API_ISOLATION_GROUP => API::IsolationGroup,
+            sai_sys::_sai_api_t_SAI_API_NAT => API::NAT,
+            sai_sys::_sai_api_t_SAI_API_COUNTER => API::Counter,
+            sai_sys::_sai_api_t_SAI_API_DEBUG_COUNTER => API::DebugCounter,
+            sai_sys::_sai_api_t_SAI_API_MACSEC => API::MACSEC,
+            sai_sys::_sai_api_t_SAI_API_SYSTEM_PORT => API::SystemPort,
+            sai_sys::_sai_api_t_SAI_API_MY_MAC => API::MyMac,
+            sai_sys::_sai_api_t_SAI_API_IPSEC => API::IPSEC,
+            sai_sys::_sai_api_t_SAI_API_GENERIC_PROGRAMMABLE => API::GenericProgrammable,
+            v => API::Unknown(v),
+        }
+    }
+}
+
+impl From<API> for sai_api_t {
+    fn from(value: API) -> Self {
+        match value {
+            API::Unspecified => sai_sys::_sai_api_t_SAI_API_UNSPECIFIED,
+            API::Switch => sai_sys::_sai_api_t_SAI_API_SWITCH,
+            API::Port => sai_sys::_sai_api_t_SAI_API_PORT,
+            API::FDB => sai_sys::_sai_api_t_SAI_API_FDB,
+            API::VLAN => sai_sys::_sai_api_t_SAI_API_VLAN,
+            API::VirtualRouter => sai_sys::_sai_api_t_SAI_API_VIRTUAL_ROUTER,
+            API::Route => sai_sys::_sai_api_t_SAI_API_ROUTE,
+            API::NextHop => sai_sys::_sai_api_t_SAI_API_NEXT_HOP,
+            API::NextHopGroup => sai_sys::_sai_api_t_SAI_API_NEXT_HOP_GROUP,
+            API::RouterInterface => sai_sys::_sai_api_t_SAI_API_ROUTER_INTERFACE,
+            API::Neighbor => sai_sys::_sai_api_t_SAI_API_NEIGHBOR,
+            API::ACL => sai_sys::_sai_api_t_SAI_API_ACL,
+            API::HostIf => sai_sys::_sai_api_t_SAI_API_HOSTIF,
+            API::Mirror => sai_sys::_sai_api_t_SAI_API_MIRROR,
+            API::SamplePacket => sai_sys::_sai_api_t_SAI_API_SAMPLEPACKET,
+            API::STP => sai_sys::_sai_api_t_SAI_API_STP,
+            API::LAG => sai_sys::_sai_api_t_SAI_API_LAG,
+            API::Policer => sai_sys::_sai_api_t_SAI_API_POLICER,
+            API::WRED => sai_sys::_sai_api_t_SAI_API_WRED,
+            API::QosMap => sai_sys::_sai_api_t_SAI_API_QOS_MAP,
+            API::Queue => sai_sys::_sai_api_t_SAI_API_QUEUE,
+            API::Scheduler => sai_sys::_sai_api_t_SAI_API_SCHEDULER,
+            API::SchedulerGroup => sai_sys::_sai_api_t_SAI_API_SCHEDULER_GROUP,
+            API::Buffer => sai_sys::_sai_api_t_SAI_API_BUFFER,
+            API::Hash => sai_sys::_sai_api_t_SAI_API_HASH,
+            API::UDF => sai_sys::_sai_api_t_SAI_API_UDF,
+            API::Tunnel => sai_sys::_sai_api_t_SAI_API_TUNNEL,
+            API::L2MC => sai_sys::_sai_api_t_SAI_API_L2MC,
+            API::IPMC => sai_sys::_sai_api_t_SAI_API_IPMC,
+            API::RpfGroup => sai_sys::_sai_api_t_SAI_API_RPF_GROUP,
+            API::L2mcGroup => sai_sys::_sai_api_t_SAI_API_L2MC_GROUP,
+            API::IpmcGroup => sai_sys::_sai_api_t_SAI_API_IPMC_GROUP,
+            API::McastFdb => sai_sys::_sai_api_t_SAI_API_MCAST_FDB,
+            API::Bridge => sai_sys::_sai_api_t_SAI_API_BRIDGE,
+            API::TAM => sai_sys::_sai_api_t_SAI_API_TAM,
+            API::SRV6 => sai_sys::_sai_api_t_SAI_API_SRV6,
+            API::MPLS => sai_sys::_sai_api_t_SAI_API_MPLS,
+            API::DTEL => sai_sys::_sai_api_t_SAI_API_DTEL,
+            API::BFD => sai_sys::_sai_api_t_SAI_API_BFD,
+            API::IsolationGroup => sai_sys::_sai_api_t_SAI_API_ISOLATION_GROUP,
+            API::NAT => sai_sys::_sai_api_t_SAI_API_NAT,
+            API::Counter => sai_sys::_sai_api_t_SAI_API_COUNTER,
+            API::DebugCounter => sai_sys::_sai_api_t_SAI_API_DEBUG_COUNTER,
+            API::MACSEC => sai_sys::_sai_api_t_SAI_API_MACSEC,
+            API::SystemPort => sai_sys::_sai_api_t_SAI_API_SYSTEM_PORT,
+            API::MyMac => sai_sys::_sai_api_t_SAI_API_MY_MAC,
+            API::IPSEC => sai_sys::_sai_api_t_SAI_API_IPSEC,
+            API::GenericProgrammable => sai_sys::_sai_api_t_SAI_API_GENERIC_PROGRAMMABLE,
+            API::Unknown(v) => v,
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct SAI {
     apis: sai_apis_t,
 }
 
 impl SAI {
+    pub fn api_version() -> Result<u64, Status> {
+        let mut version: sai_api_version_t = 0;
+        unsafe {
+            match sai_query_api_version(&mut version) {
+                0 => Ok(version),
+                v => Err(Status::from(v)),
+            }
+        }
+    }
+
+    pub fn log_set(api: API, lvl: LogLevel) -> Result<(), Status> {
+        unsafe {
+            match sai_log_set(sai_api_t::from(api), sai_log_level_t::from(lvl)) {
+                0 => Ok(()),
+                v => Err(Status::from(v)),
+            }
+        }
+    }
+
+    pub fn log_set_all(lvl: LogLevel) -> Result<(), Vec<(API, Status)>> {
+        let mut ret_err: Vec<(API, Status)> = vec![];
+        for api in 1.._sai_api_t_SAI_API_MAX {
+            unsafe {
+                match sai_log_set(api, sai_log_level_t::from(lvl)) {
+                    0 => {}
+                    v => {
+                        ret_err.push((API::from(api), Status::from(v)));
+                    }
+                };
+            }
+        }
+        if ret_err.is_empty() {
+            Ok(())
+        } else {
+            Err(ret_err)
+        }
+    }
+
     pub fn new(profile: Vec<(CString, CString)>) -> Result<SAI, InitError> {
         let init_lock = SAI_INITIALIZED.try_lock();
         if let Ok(mut sai_initialized) = init_lock {
@@ -319,6 +545,22 @@ impl Drop for SAI {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn sai_log_set() {
+        let res = SAI::log_set_all(LogLevel::Info);
+        assert!(res.is_ok());
+        let res = SAI::log_set(API::Switch, LogLevel::Debug);
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn sai_query_api_version() {
+        let res = SAI::api_version();
+        assert!(res.is_ok());
+        // TODO: this works only for certain SAIs obviously
+        assert!(res.unwrap() >= 11100);
+    }
 
     #[test]
     fn sai_new_and_drop() {
