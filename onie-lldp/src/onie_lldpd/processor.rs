74d58c8e17d0898aeba7755b4a5f5f1ea9e79b05
Marcus Heese <marcus@githedgehog.com>
2023-11-21 14:58:29 -0800

    first version of onie-lldp

---
+++ onie-lldp/src/onie_lldpd/processor.rs
@@ -0,0 +1,384 @@
+mod netlink;
+
+use std::sync::mpsc::channel;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
+use std::sync::Arc;
+use std::thread;
+
+use onie_sai_rpc::onie_sai;
+use onie_sai_rpc::wrap_message_field;
+
+use thiserror::Error;
+
+use crate::onie_lldpd::lldp::LLDPTLVs;
+use crate::onie_lldpd::lldp::NetworkConfig;
+
+use super::lldp::LLDPSocket;
+
+#[derive(Error, Debug)]
+pub(crate) enum ProcessError {
+    #[error("failed to get interface: {0}")]
+    GetInterfaceError(std::io::Error),
+
+    #[error("failed to get interface index for '{0}'")]
+    NoSuchInterfaceError(String),
+}
+
+pub(crate) enum ProcessRequest {
+    Shutdown,
+    Version(
+        (
+            onie_sai::VersionRequest,
+            Sender<Result<onie_sai::VersionResponse, ProcessError>>,
+        ),
+    ),
+    IsInitialDiscoveryFinished(
+        (
+            onie_sai::IsInitialDiscoveryFinishedRequest,
+            Sender<Result<onie_sai::IsInitialDiscoveryFinishedResponse, ProcessError>>,
+        ),
+    ),
+    LLDPTLVsReceived((u32, LLDPTLVs)),
+    LLDPNetworkConfigReceived((u32, NetworkConfig)),
+    LLDPStatus(
+        (
+            onie_sai::LLDPStatusRequest,
+            Sender<Result<onie_sai::LLDPStatusResponse, ProcessError>>,
+        ),
+    ),
+    LLDPNetworkConfig(
+        (
+            onie_sai::LLDPNetworkConfigRequest,
+            Sender<Result<onie_sai::LLDPNetworkConfigResponse, ProcessError>>,
+        ),
+    ),
+}
+
+pub(crate) struct Processor {
+    rx: Receiver<ProcessRequest>,
+    tx: Sender<ProcessRequest>,
+    hifs: Vec<HostInterface>,
+}
+
+impl Processor {
+    pub(crate) fn new() -> anyhow::Result<Self> {
+        // the processor channel
+        let (tx, rx) = channel();
+
+        let mut hifs = Vec::new();
+        for (idx, ifname) in netlink::get_interfaces()? {
+            let mut hif = HostInterface {
+                name: ifname,
+                idx: idx,
+                lldp_socket: None,
+                lldp_tlvs: None,
+                lldp_network_config: None,
+            };
+            hif.start_lldp_recv_thread(tx.clone());
+            hifs.push(hif);
+        }
+
+        Ok(Processor {
+            rx: rx,
+            tx: tx,
+            hifs: hifs,
+        })
+    }
+
+    pub(crate) fn get_sender(&self) -> Sender<ProcessRequest> {
+        self.tx.clone()
+    }
+
+    pub(crate) fn process(self) {
+        let mut p = self;
+        while let Ok(req) = p.rx.recv() {
+            match req {
+                // shut down processor
+                ProcessRequest::Shutdown => return,
+
+                // all RPC request handling
+                ProcessRequest::Version((r, resp_tx)) => {
+                    let resp = p.process_version_request(r);
+                    if let Err(e) = resp_tx.send(resp) {
+                        log::error!("failed to send version response to rpc server: {:?}", e);
+                    };
+                }
+                ProcessRequest::IsInitialDiscoveryFinished((r, resp_tx)) => {
+                    let resp = p.process_is_initial_discovery_finished_request(r);
+                    if let Err(e) = resp_tx.send(resp) {
+                        log::error!(
+                            "failed to send is initial discovery finished response to rpc server: {:?}",
+                            e
+                        );
+                    };
+                }
+                ProcessRequest::LLDPStatus((r, resp_tx)) => {
+                    let resp = p.process_lldp_status_request(r);
+                    if let Err(e) = resp_tx.send(resp) {
+                        log::error!("failed to send lldp status response to rpc server: {e:?}");
+                    };
+                }
+                ProcessRequest::LLDPNetworkConfig((r, resp_tx)) => {
+                    let resp = p.process_lldp_network_config_request(r);
+                    if let Err(e) = resp_tx.send(resp) {
+                        log::error!(
+                            "failed to send lldp network config response to rpc server: {e:?}"
+                        );
+                    };
+                }
+
+                // internal events
+                ProcessRequest::LLDPTLVsReceived((if_idx, tlvs)) => {
+                    p.process_lldp_tlvs_received(if_idx, tlvs)
+                }
+                ProcessRequest::LLDPNetworkConfigReceived((if_idx, config)) => {
+                    p.process_lldp_network_config_received(if_idx, config)
+                }
+            }
+        }
+    }
+
+    fn process_version_request(
+        &self,
+        _: onie_sai::VersionRequest,
+    ) -> Result<onie_sai::VersionResponse, ProcessError> {
+        Ok(onie_sai::VersionResponse {
+            onie_said_version: "0.1.0".to_string(),
+            ..Default::default()
+        })
+    }
+
+    fn process_is_initial_discovery_finished_request(
+        &self,
+        _: onie_sai::IsInitialDiscoveryFinishedRequest,
+    ) -> Result<onie_sai::IsInitialDiscoveryFinishedResponse, ProcessError> {
+        // NOTE: just a fake result in onie-lldpd
+        Ok(onie_sai::IsInitialDiscoveryFinishedResponse {
+            is_finished: true,
+            ..Default::default()
+        })
+    }
+
+    fn process_lldp_status_request(
+        &self,
+        req: onie_sai::LLDPStatusRequest,
+    ) -> Result<onie_sai::LLDPStatusResponse, ProcessError> {
+        let if_idx = netlink::get_interface_index(req.device.as_str())
+            .map_err(|e| ProcessError::GetInterfaceError(e))?;
+
+        // this should not be necessary actually, but we'll keep this just in case
+        if if_idx == 0 {
+            return Err(ProcessError::NoSuchInterfaceError(req.device.clone()));
+        }
+        log::debug!("LLDPStatusRequest: looking for LLDP TLVs for interface {if_idx}");
+
+        for hif in self.hifs.iter() {
+            if hif.idx == if_idx {
+                if let Some(ref lldp_tlvs) = hif.lldp_tlvs {
+                    return Ok(onie_sai::LLDPStatusResponse {
+                        tlvs: lldp_tlvs.to_strings(),
+                        packet_received: true,
+                        ..Default::default()
+                    });
+                }
+                log::debug!("LLDPStatusRequest: interface found, but no LLDP TLVs found for interface {if_idx}");
+            }
+        }
+
+        log::debug!(
+            "LLDPStatusRequest: interface not found or no LLDP TLVs found for interface {if_idx}"
+        );
+        Ok(onie_sai::LLDPStatusResponse {
+            packet_received: false,
+            ..Default::default()
+        })
+    }
+
+    fn process_lldp_network_config_request(
+        &self,
+        req: onie_sai::LLDPNetworkConfigRequest,
+    ) -> Result<onie_sai::LLDPNetworkConfigResponse, ProcessError> {
+        let if_idx = netlink::get_interface_index(req.device.as_str())
+            .map_err(|e| ProcessError::GetInterfaceError(e))?;
+
+        // this should not be necessary actually, but we'll keep this just in case
+        if if_idx == 0 {
+            return Err(ProcessError::NoSuchInterfaceError(req.device.clone()));
+        }
+        log::debug!(
+            "LLDPNetworkConfigRequest: looking for LLDP network config for interface {if_idx}"
+        );
+
+        for hif in self.hifs.iter() {
+            if hif.idx == if_idx {
+                if let Some(ref config) = hif.lldp_network_config {
+                    return Ok(onie_sai::LLDPNetworkConfigResponse {
+                        network_config: wrap_message_field(Some(config.clone().into())),
+                        ..Default::default()
+                    });
+                }
+                log::debug!("LLDPNetworkConfigRequest: interface found, but no LLDP network config found for interface {if_idx}");
+            }
+        }
+
+        log::debug!("LLDPNetworkConfigRequest: interface not found or no LLDP network config found for interface {if_idx}");
+        Ok(onie_sai::LLDPNetworkConfigResponse {
+            network_config: wrap_message_field(None),
+            ..Default::default()
+        })
+    }
+
+    fn process_lldp_tlvs_received(&mut self, if_idx: u32, lldp_tlvs: LLDPTLVs) {
+        let if_name = netlink::get_interface_name(if_idx).unwrap_or("unknown".to_string());
+        // find the host interface
+        // and update the TLVs in there
+        let mut found = false;
+
+        for hif in self.hifs.iter_mut() {
+            if hif.idx == if_idx {
+                found = true;
+                hif.lldp_tlvs = Some(lldp_tlvs);
+                break;
+            }
+        }
+
+        if !found {
+            log::warn!("host interface {if_name} ({if_idx}) not found during LLDP TLVs event. Discovered LLDP TLVs were not stored.");
+        }
+    }
+
+    fn process_lldp_network_config_received(&mut self, if_idx: u32, config: NetworkConfig) {
+        let if_name = netlink::get_interface_name(if_idx).unwrap_or("unknown".to_string());
+        // find the host interface
+        // and update the network config in there
+        let mut found = false;
+
+        for hif in self.hifs.iter_mut() {
+            if hif.idx == if_idx {
+                found = true;
+                hif.lldp_network_config = Some(config);
+                break;
+            }
+        }
+
+        if !found {
+            log::warn!("host interface {if_name} ({if_idx}) not found during LLDP network config event. Discovered LLDP Network Config was not stored.");
+        }
+    }
+}
+
+impl Drop for Processor {
+    fn drop(&mut self) {
+        for hif in self.hifs.iter_mut() {
+            hif.stop_lldp_recv_thread();
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct HostInterface {
+    pub(crate) name: String,
+    pub(crate) idx: u32,
+    pub(crate) lldp_socket: Option<Arc<LLDPSocket>>,
+    pub(crate) lldp_tlvs: Option<LLDPTLVs>,
+    pub(crate) lldp_network_config: Option<NetworkConfig>,
+}
+
+impl HostInterface {
+    fn start_lldp_recv_thread(&mut self, processor_sender: Sender<ProcessRequest>) {
+        if self.lldp_socket.is_none() {
+            match LLDPSocket::new(self.idx as i32) {
+                Ok(socket) => {
+                    let socket = Arc::new(socket);
+                    self.lldp_socket = Some(socket.clone());
+                    let hif_idx = self.idx;
+                    let hif_name = self.name.clone();
+                    thread::spawn(move || {
+                        log::debug!("Host Interface {hif_name}: LLDP receive thread started");
+                        loop {
+                            // this blocks until we receive a new packet on the socket
+                            match socket.recv_packet() {
+                                Ok(pkt) => {
+                                    log::debug!("Host Interface {hif_name}: received LLDP packet");
+                                    // parse the LLDP TLVs from the packet
+                                    let lldp_tlvs: LLDPTLVs = match pkt.as_slice().try_into() {
+                                        Ok(v) => v,
+                                        Err(err) => {
+                                            log::error!("Host Interface {}: failed to read and/or parse LLDP packet: {:?}", hif_name, err);
+                                            continue;
+                                        }
+                                    };
+
+                                    // send the LLDP TLVs to the processor thread
+                                    // this is for general LLDP information that we received on this host interface
+                                    // which can be queried through onie-saictl
+                                    if let Err(e) =
+                                        processor_sender.send(ProcessRequest::LLDPTLVsReceived((
+                                            hif_idx,
+                                            lldp_tlvs.clone(),
+                                        )))
+                                    {
+                                        log::error!(
+                                            "Host Interface {}: failed to send LLDP TLVs to processor thread: {:?}",
+                                            hif_name,
+                                            e
+                                        );
+                                    }
+
+                                    // and try to get the network config from the LLDP TLVs
+                                    if let Some(network_config) = lldp_tlvs.get_hh_network_config()
+                                    {
+                                        // store network config for this interface by sending it to the processor thread
+                                        if let Err(e) = processor_sender.send(
+                                            ProcessRequest::LLDPNetworkConfigReceived((
+                                                hif_idx,
+                                                network_config.clone(),
+                                            )),
+                                        ) {
+                                            log::error!(
+                                                "Host Interface {}: failed to send LLDP network config to processor thread: {:?}",
+                                                hif_name,
+                                                e
+                                            );
+                                        }
+                                    }
+                                }
+                                Err(e) => {
+                                    // an error receiving most likely means that the socket was closed
+                                    // we simply abort the thread for now until we know exactly what error to watch for
+                                    log::error!(
+                                        "Host Interface {}: failed to receive LLDP packet: {:?}",
+                                        hif_name,
+                                        e
+                                    );
+                                    break;
+                                }
+                            }
+                        }
+                        log::debug!("Host Interface {}: LLDP receive thread stopped", hif_name);
+                    });
+                }
+                Err(e) => {
+                    log::error!(
+                        "Host Interface {}: failed to create LLDP socket: {:?}",
+                        self.name,
+                        e
+                    );
+                }
+            }
+        }
+    }
+
+    fn stop_lldp_recv_thread(&mut self) {
+        if let Some(socket) = self.lldp_socket.take() {
+            socket.ref_close();
+        }
+    }
+}
+
+impl std::fmt::Display for HostInterface {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} ({})", self.name, self.idx)
+    }
+}
---
+++ onie-lldp/src/onie_lldpd/processor/netlink.rs
@@ -0,0 +1,313 @@
+// use libc::RTMGRP_IPV4_IFADDR;
+// use libc::RTMGRP_IPV6_IFADDR;
+use netlink_packet_core::constants::NLM_F_ACK;
+use netlink_packet_core::constants::NLM_F_REQUEST;
+use netlink_packet_core::NetlinkHeader;
+use netlink_packet_core::NetlinkMessage;
+use netlink_packet_core::NetlinkPayload;
+// use netlink_packet_route::rtnl::address::AddressMessage;
+// use netlink_packet_route::rtnl::nlas::address::Nla;
+use netlink_packet_route::LinkMessage;
+use netlink_packet_route::RtnlMessage;
+use netlink_packet_utils::DecodeError;
+use netlink_sys::protocols::NETLINK_ROUTE;
+use netlink_sys::Socket;
+use netlink_sys::SocketAddr;
+use std::ffi::CStr;
+// use std::net::IpAddr;
+// use std::sync::mpsc::Sender;
+// use std::thread;
+
+// use super::ProcessRequest;
+
+// pub(crate) fn netlink_addr_monitor(
+//     proc_tx: Sender<ProcessRequest>,
+// ) -> Result<thread::JoinHandle<()>, std::io::Error> {
+//     let mut socket = Socket::new(NETLINK_ROUTE)?;
+//     let addr = SocketAddr::new(0, (RTMGRP_IPV4_IFADDR | RTMGRP_IPV6_IFADDR) as u32);
+//     socket.bind(&addr)?;
+
+//     let mut buf = vec![0; 8192];
+//     let mut off = 0;
+
+//     Ok(thread::spawn(move || {
+//         loop {
+//             let size = match socket.recv(&mut &mut buf[..], 0) {
+//                 Ok(v) => v,
+//                 Err(e) => {
+//                     log::error!(
+//                         "netlink addr monitor: failed to receive from netlink socket: {e:?}"
+//                     );
+//                     continue;
+//                 }
+//             };
+
+//             // there is no guarantee that a single receive call gives us only one netlink message
+//             // so we need to loop and try to deserialize multiple messages
+//             loop {
+//                 let packet: NetlinkMessage<RtnlMessage> = match NetlinkMessage::deserialize(
+//                     &buf[off..],
+//                 ) {
+//                     Ok(v) => v,
+//                     Err(e) => {
+//                         log::error!("netlink addr monitor: received invalid netlink message, failed to deserialize: {e:?}");
+//                         break;
+//                     }
+//                 };
+
+//                 match packet.payload {
+//                     NetlinkPayload::InnerMessage(RtnlMessage::NewAddress(v)) => {
+//                         match convert(v) {
+//                             Ok(v) => {
+//                                 let _ = proc_tx.send(ProcessRequest::NetlinkAddrAdded(v));
+//                             }
+//                             Err(e) => {
+//                                 log::error!("netlink addr monitor: failed to convert address message: {e:?}");
+//                             }
+//                         };
+//                     }
+//                     NetlinkPayload::InnerMessage(RtnlMessage::DelAddress(v)) => {
+//                         match convert(v) {
+//                             Ok(v) => {
+//                                 let _ = proc_tx.send(ProcessRequest::NetlinkAddrRemoved(v));
+//                             }
+//                             Err(e) => {
+//                                 log::error!("netlink addr monitor: failed to convert address message: {e:?}");
+//                             }
+//                         };
+//                     }
+//                     NetlinkPayload::Error(e) => {
+//                         log::error!(
+//                         "netlink addr monitor: received error message from netlink socket: {e:?}"
+//                     );
+//                     }
+//                     v => {
+//                         log::warn!("netlink addr monitor: received unexpected message from netlink socket: {v:?}");
+//                     }
+//                 }
+
+//                 off += packet.header.length as usize;
+//                 if off == size || packet.header.length == 0 {
+//                     off = 0;
+//                     break;
+//                 }
+//             }
+//         }
+//     }))
+// }
+
+// #[derive(Debug)]
+// pub(crate) enum AddressMessageConversionError {
+//     InvalidFamily(i32),
+//     InvalidAddress(core::array::TryFromSliceError),
+//     NoNLAAddress,
+// }
+
+// fn convert(am: AddressMessage) -> Result<(u32, IpAddr), AddressMessageConversionError> {
+//     match am.header.family as i32 {
+//         libc::AF_INET => am
+//             .nlas
+//             .iter()
+//             .find_map(|nla| match nla {
+//                 Nla::Address(addr) => Some(addr),
+//                 _ => None,
+//             })
+//             .ok_or(AddressMessageConversionError::NoNLAAddress)
+//             .and_then(|addr| match <[u8; 4]>::try_from(addr.as_slice()) {
+//                 Ok(addr) => Ok(IpAddr::from(addr)),
+//                 Err(e) => Err(AddressMessageConversionError::InvalidAddress(e)),
+//             })
+//             .map(|addr| (am.header.index, addr)),
+//         libc::AF_INET6 => am
+//             .nlas
+//             .iter()
+//             .find_map(|nla| match nla {
+//                 Nla::Address(addr) => Some(addr),
+//                 _ => None,
+//             })
+//             .ok_or(AddressMessageConversionError::NoNLAAddress)
+//             .and_then(|addr| match <[u8; 16]>::try_from(addr.as_slice()) {
+//                 Ok(addr) => Ok(IpAddr::from(addr)),
+//                 Err(e) => Err(AddressMessageConversionError::InvalidAddress(e)),
+//             })
+//             .map(|addr| (am.header.index, addr)),
+//         family => Err(AddressMessageConversionError::InvalidFamily(family)),
+//     }
+// }
+
+pub(crate) fn get_interface_name(index: u32) -> Result<String, std::io::Error> {
+    let mut buf = [0u8; libc::IFNAMSIZ];
+    let ret = unsafe { libc::if_indextoname(index, buf.as_mut_ptr() as *mut libc::c_char) };
+    if ret.is_null() {
+        return Err(std::io::Error::last_os_error());
+    }
+    let name = unsafe { CStr::from_ptr(buf.as_ptr() as *const libc::c_char) };
+    Ok(name.to_string_lossy().into_owned())
+}
+
+pub(crate) fn get_interface_index(name: &str) -> Result<u32, std::io::Error> {
+    let name = std::ffi::CString::new(name).unwrap();
+    let ret = unsafe { libc::if_nametoindex(name.as_ptr()) };
+    if ret <= 0 {
+        return Err(std::io::Error::last_os_error());
+    }
+    Ok(ret)
+}
+
+pub(crate) fn get_interfaces() -> Result<Vec<(u32, String)>, std::io::Error> {
+    let mut ptr = unsafe { libc::if_nameindex() };
+    if ptr.is_null() {
+        return Err(std::io::Error::last_os_error());
+    }
+    let mut ret: Vec<(u32, String)> = Vec::new();
+    loop {
+        let if_nameindex = unsafe { *ptr };
+        if if_nameindex.if_index == 0 {
+            break;
+        }
+        let name = unsafe { CStr::from_ptr(if_nameindex.if_name) };
+        let name = name.to_string_lossy().into_owned();
+        ret.push((if_nameindex.if_index, name));
+        ptr = unsafe { ptr.offset(1) };
+    }
+
+    // // iterate over the returned list and build the vector for return
+    // let count = unsafe { attr.value.u32list.count };
+    // let list = unsafe { attr.value.u32list.list };
+    // let mut ret: Vec<u32> = Vec::with_capacity(count as usize);
+    // for i in 0..count {
+    //     let speed: u32 = unsafe { *list.offset(i as isize) };
+    //     ret.push(speed);
+    // }
+    Ok(ret)
+}
+
+#[derive(Debug)]
+pub(crate) enum SetLinkError {
+    IOError(std::io::Error),
+    NetlinkDecodeError(DecodeError),
+    UnexpectedNetlinkMessage(u16),
+    NetlinkError(i32),
+}
+
+impl std::error::Error for SetLinkError {}
+
+impl std::fmt::Display for SetLinkError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SetLinkError::IOError(e) => write!(f, "IO error: {}", e),
+            SetLinkError::NetlinkDecodeError(e) => write!(f, "netlink decoding error: {}", e),
+            SetLinkError::UnexpectedNetlinkMessage(v) => {
+                write!(f, "unexpected netlink message received: {}", v)
+            }
+            SetLinkError::NetlinkError(v) => write!(f, "netlink error: {}", v),
+        }
+    }
+}
+
+impl From<std::io::Error> for SetLinkError {
+    fn from(e: std::io::Error) -> Self {
+        SetLinkError::IOError(e)
+    }
+}
+
+impl From<DecodeError> for SetLinkError {
+    fn from(e: DecodeError) -> Self {
+        SetLinkError::NetlinkDecodeError(e)
+    }
+}
+
+#[allow(dead_code)]
+pub(crate) fn set_link_status(index: u32, oper_status: bool) -> Result<(), SetLinkError> {
+    let mut socket = Socket::new(NETLINK_ROUTE)?;
+    let sock_addr = socket.bind_auto()?;
+    let port_number = sock_addr.port_number();
+    socket.connect(&SocketAddr::new(0, 0))?;
+
+    // GET LINK - get the link first before we modify it
+    // build request
+    let mut hdr = NetlinkHeader::default();
+    hdr.flags = NLM_F_REQUEST;
+    hdr.port_number = port_number;
+    let mut lm = LinkMessage::default();
+    lm.header.index = index;
+    let mut req = NetlinkMessage::new(hdr, NetlinkPayload::from(RtnlMessage::GetLink(lm)));
+    req.finalize();
+
+    // serialize the request
+    let mut buf = vec![0u8; req.header.length as usize];
+    req.serialize(buf.as_mut_slice());
+
+    // send the request
+    let _ = socket.send(buf.as_slice(), 0)?;
+
+    // receive the response
+    let mut buf = vec![0u8; 8192];
+    let _ = socket.recv(&mut &mut buf[..], 0)?;
+
+    // deserialize the response
+    // we are expecting an NLMSG_ERROR message without an error (which equals an ACK)
+    let resp = <NetlinkMessage<RtnlMessage>>::deserialize(&buf.as_slice())?;
+    let current_lm = match resp.payload {
+        NetlinkPayload::InnerMessage(RtnlMessage::GetLink(v)) => v,
+        NetlinkPayload::InnerMessage(RtnlMessage::NewLink(v)) => v,
+        v => {
+            println!("{v:?}");
+            return Err(SetLinkError::UnexpectedNetlinkMessage(v.message_type()));
+        }
+    };
+
+    // SET LINK - modify the link now
+    // build our netlink request
+    // we take the current link message and modify the flags
+    // NOTE: clone() does not work, it will panic during serialization
+    let mut hdr = NetlinkHeader::default();
+    hdr.flags = NLM_F_REQUEST | NLM_F_ACK;
+    hdr.port_number = port_number;
+    let mut lm = LinkMessage::default();
+    lm.header.interface_family = current_lm.header.interface_family;
+    lm.header.index = index;
+    lm.header.link_layer_type = current_lm.header.link_layer_type;
+    lm.header.flags = current_lm.header.flags;
+    lm.header.change_mask = current_lm.header.change_mask;
+    if oper_status {
+        lm.header.flags |= libc::IFF_UP as u32;
+    } else {
+        lm.header.flags &= !(libc::IFF_UP as u32);
+    }
+    let mut req = NetlinkMessage::new(hdr, NetlinkPayload::from(RtnlMessage::SetLink(lm)));
+    req.finalize();
+
+    // serialize the request
+    let mut buf = vec![0u8; req.header.length as usize];
+    req.serialize(buf.as_mut_slice());
+
+    // send the request
+    let _ = socket.send(buf.as_slice(), 0)?;
+
+    // receive the response
+    let mut buf = vec![0u8; 4096];
+    let _ = socket.recv(&mut &mut buf[..], 0)?;
+
+    // deserialize the response
+    // we are expecting an NLMSG_ERROR message without an error (which equals an ACK)
+    let resp = <NetlinkMessage<RtnlMessage>>::deserialize(&buf.as_slice())?;
+    match resp.payload {
+        NetlinkPayload::Error(err_msg) => match err_msg.code {
+            None => Ok(()),
+            Some(code) => Err(SetLinkError::NetlinkError(code.into())),
+        },
+        v => Err(SetLinkError::UnexpectedNetlinkMessage(v.message_type())),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_interfaces() {
+        let interfaces = get_interfaces().unwrap();
+        println!("{:#?}", interfaces);
+    }
+}
